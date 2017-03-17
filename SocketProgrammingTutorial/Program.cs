59ff629c7451621e5f7bc1e0a7c27f9a29d45d14
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-03-17 14:15:27 +0700

    Works, but isn't very new.

--- SocketProgrammingTutorial/Program.cs
+++ SocketProgrammingTutorial/Program.cs
@@ -1,15 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SocketProgrammingTutorial
+namespace SocketProgrammingTutorial1_Server
 {
     class Program
     {
         static void Main(string[] args)
         {
+            TcpListener serverSocket = new TcpListener(8888);
+            int requestCount = 0;
+            TcpClient clientSocket = default(TcpClient);
+            serverSocket.Start();
+            Console.WriteLine(" >> Server Started");
+            clientSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine(" >> Accept connection from Client");
+            requestCount = 0;
+
+            while (true)
+            {
+                try
+                {
+                    requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    byte[] bytesFrom = new byte[10025];
+                    networkStream.Read(bytesFrom, 0, clientSocket.ReceiveBufferSize);
+
+                    string dataFromClient = Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    Console.WriteLine(" >> Data Data From Client - " + dataFromClient);
+
+                    string serverResponse = "Last message from client" + dataFromClient;
+                    byte[] sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + serverResponse);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    break;
+                }
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine( " >> exit");
+            Console.ReadLine();
         }
     }
 }
---
+++ SocketProgrammingTutorial1_Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketProgrammingTutorial1_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient clientSocket = new TcpClient();
+            Console.WriteLine(" >> Client Started");
+
+            clientSocket.Connect("127.0.0.1", 8888);
+            Console.WriteLine(" >> Client Socket Program -  server Connected.... ");
+
+            while (true)
+            {
+                string clientResponse = Console.ReadLine();
+
+                NetworkStream serverStream = clientSocket.GetStream();
+                byte[] outStream = Encoding.ASCII.GetBytes(clientResponse + "$");
+
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+
+                byte[] inStream = new byte[10025];
+                serverStream.Read(inStream, 0, clientSocket.ReceiveBufferSize);
+                string returnedData = Encoding.ASCII.GetString(inStream);
+                Console.WriteLine(" >> " + returnedData);
+            }
+        }
+    }
+}
